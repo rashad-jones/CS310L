78d66ebadeaae840ae98ec29d4e33f9c44efe6b5
rashad-jones <rashad.jones@morehouse.edu>
2025-09-03 03:11:05 +0000

    finished lab 1

---
+++ ShapeMaker-Lab/build/CMakeFiles/ShapeIt.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/CS310L/ShapeMaker-Lab/main.cpp" "CMakeFiles/ShapeIt.dir/main.cpp.o" "gcc" "CMakeFiles/ShapeIt.dir/main.cpp.o.d"
+  "/workspaces/CS310L/ShapeMaker-Lab/shapemaker.cpp" "CMakeFiles/ShapeIt.dir/shapemaker.cpp.o" "gcc" "CMakeFiles/ShapeIt.dir/shapemaker.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ShapeMaker-Lab/build/CMakeFiles/ShapeIt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShapeIt.dir/main.cpp.o"
+  "CMakeFiles/ShapeIt.dir/main.cpp.o.d"
+  "CMakeFiles/ShapeIt.dir/shapemaker.cpp.o"
+  "CMakeFiles/ShapeIt.dir/shapemaker.cpp.o.d"
+  "ShapeIt"
+  "ShapeIt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShapeIt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
